72e23fe56ad621406aa649b2add2ddbeccc438e7
Henrique Souza Nascimento <hesnascimento@gmail.com>
2019-04-13 09:18:09 -0300

    Commit inicial

---
+++ src/scenes/main/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default () => (
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-6 offset-sm-3">
+        <img
+          style={{
+            maxWidth:'100%',
+          }}
+          src="/images/megaburguer_logo.png"
+          alt="MEGABURGUER - Do tamanho da sua fome" />
+      </div>
+    </div>
+    <div
+      className="row"
+      style={{
+        borderBottomStyle: 'solid',
+        borderBottomWidth: '1px',
+        borderBottomColor: '#CCCCCCCC',
+      }}>
+      <div
+        className="col-sm"
+        style={{
+          marginTop: '2em',
+          fontFamily: 'Roboto, sans-serif',
+          fontWeight: 300,
+          color: '#FFFFFF',
+          fontSize: '23px',
+        }}>
+        Escolha o Burguer:
+      </div>
+    </div>
+  </div>
+)
