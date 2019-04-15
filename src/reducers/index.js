84d2807ad7e4e90d507794e80a4322a75f545015
Henrique Souza Nascimento <hesnascimento@gmail.com>
2019-04-15 00:54:18 -0300

    Mudar nome Ingredientes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import ingredientes from './ingredientes';
+import ingredientes from './Ingredientes';
 
 export default combineReducers({
   ingredientes,
